37ba5defe2c9b95b5629cb811a3d50e68cc6929a
Neilio3264 <neilrathod12@gmail.com>
2023-09-07 11:28:46 -0500

    Create basic thread safe stack implementation

---
+++ Section 2/Thread Safe Stack/include/stack.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <mutex>
+#include <stack>
+#include <thread>
+
+template <typename T>
+class tsStack
+{
+private:
+    std::stack<T> stk;
+    std::mutex m;
+
+public:
+    tsStack();
+    ~tsStack();
+
+    void push(T element)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        stk.push(element);
+    }
+
+    void pop()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        stk.pop();
+    }
+
+    T &top()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        return stk.top();
+    }
+
+    bool empty()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        return stk.empty();
+    }
+
+    int size()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        return stk.size();
+    }
+};
